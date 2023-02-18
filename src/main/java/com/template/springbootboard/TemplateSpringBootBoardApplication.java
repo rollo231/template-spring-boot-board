b33275136d6926966f3269c2425a6eb802f893ba
sangwon lee <231rollo@gmail.com>
2023-02-18 12:58:34 +0900

    #6 - 스프링 부트 프로젝트 시작
    스프링 부트 프로젝트 초기화
    
    들어간 초기 기술은 : 
    
    * 스프링 웹
    * 엑츄에이터
    * 데브툴즈
    * 롬복

---
+++ src/main/java/com/template/springbootboard/TemplateSpringBootBoardApplication.java
@@ -0,0 +1,13 @@
+package com.template.springbootboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TemplateSpringBootBoardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TemplateSpringBootBoardApplication.class, args);
+	}
+
+}
